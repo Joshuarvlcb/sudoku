daf85e57384ff4632176ec3603f20818941f78f0
josh <joshuarvlcb@gmail.com>
2021-05-31 12:51:52 -0700

    adding logic to show if you got a incorrect square

--- script.js
+++ script.js
@@ -1,170 +1,166 @@
-const gridContainer = document.querySelector('.container');
-let gridArr = new Array(9).fill('').map((_,i) => {
-    return new Array(9).fill('').map((_,j,arr) => {
-        return null
-    })
-})
+const gridContainer = document.querySelector(".container");
+let gridArr = new Array(9).fill("").map((_, i) => {
+  return new Array(9).fill("").map((_, j, arr) => {
+    return null;
+  });
+});
 
 const _BOARD = [
-    [" ", "9", " ", " ", "4", "2", "1", "3", "6"],
-    [" ", " ", " ", "9", "6", " ", "4", "8", "5"],
-    [" ", " ", " ", "5", "8", "1", " ", " ", " "],
-   
-    [" ", " ", "4", " ", " ", " ", " ", " ", " "],
-    ["5", "1", "7", "2", " ", " ", "9", " ", " "],
-    ["6", " ", "2", " ", " ", " ", "3", "7", " "],
-   
-    ["1", " ", " ", "8", " ", "4", " ", "2", " "],
-    ["7", " ", "6", " ", " ", " ", "8", "1", " "],
-    ["3", " ", " ", " ", "9", " ", " ", " ", " "],
-  ];
-   
-  const _COARDS = [
-    [1, 1, 1, 2, 2, 2, 3, 3, 3],
-    [1, 1, 1, 2, 2, 2, 3, 3, 3],
-    [1, 1, 1, 2, 2, 2, 3, 3, 3],
-   
-    [4, 4, 4, 5, 5, 5, 6, 6, 6],
-    [4, 4, 4, 5, 5, 5, 6, 6, 6],
-    [4, 4, 4, 5, 5, 5, 6, 6, 6],
-   
-    [7, 7, 7, 8, 8, 8, 9, 9, 9],
-    [7, 7, 7, 8, 8, 8, 9, 9, 9],
-    [7, 7, 7, 8, 8, 8, 9, 9, 9],
-  ];
-   
-  const getRow = function (board, row) {
-    //return an array with all the array elements from the row
-    return board[row];
-  };
-   
-  const getColumn = function (board, column) {
-    //this return an array with all the array elements fom the column
-    let inColumn = [];
-    for (let row in board) {
-      inColumn.push(board[row][column]);
-    }
-    return inColumn;
-  };
-  const getQuad = function (board, quadNumber) {
-    //creates an array of all elements in the same quadrant
-    let inQuad = [];
-    //this cycles though all the rows
-    for (row in board) {
-      //this cycles though all the columns
-      for (col in board[row]) {
-        if (_COARDS[row][col] === quadNumber) {
-          inQuad.push(board[row][col]);
-        }
-      }
-    }
-    return inQuad;
-  };
-  console.log(getQuad(_COARDS, 1));
-  //filter through all the possible values and return all of the used values and return only the possible values of that cell
-  const getPossible = function (board, row, col) {
-    let inColumn = getColumn(board, col);
-    let inRow = getRow(board, row);
-    let inQuad = getQuad(board, _COARDS[row][col]);
-    let possible = new Array(9).fill("").map((_, i) => {
-      return i + 1;
-    });
-   
-    let notPossible = [];
-   
-    //checking through the columns horizontally
-    inRow.forEach((curr, r) => {
-      let included = false;
-      notPossible.forEach((curr, p) => {
-        if (inRow[r] == notPossible[p]) {
-          included = true;
-        }
-      });
-      if (!included) {
-        notPossible.push(inRow[r]);
-      }
-    });
-   
-    //checking through the columns vertically
-    inColumn.forEach((curr, c) => {
-      let included = false;
-      notPossible.forEach((curr, p) => {
-        if (inColumn[c] == notPossible[p]) {
-          included = true;
-        }
-      });
-      if (!included) {
-        notPossible.push(inColumn[c]);
-      }
-    });
-   
-    // [(".", "9", "4", "2", "1", "3", "6", "5", "7")];  //checking quad
-    inQuad.forEach((curr, q) => {
-      let included = false;
-      notPossible.forEach((curr, p) => {
-        if (inQuad[q] == notPossible[p]) {
-          included = true;
-        }
-      });
-      if (!included) {
-        notPossible.push(inQuad[q]);
-      }
-    });
-   
-    notPossible = notPossible.filter((curr) => curr !== " ");
-   
-    notPossible.forEach((_, n) => {
-      possible.forEach((_, p) => {
-        if (notPossible[n] == possible[p]) {
-          possible.splice(p, 1);
-        }
-      });
-    });
-    return possible
-  };
-  console.log(_BOARD)
-  console.log(gridArr)
-  const fillInCell = function (board, row, column) {
-    let possible = getPossible(board, row, column);
-   
-    if (board[row][column] == " ") {
-      if (possible.length == 1) {
-        board[row][column] = possible[0];
-      }
-    }
-  };
-  let newAnswers = new Array(9).fill(0).map((row,i) => {
-      return new Array(9).fill(9).map((col) => {
-          return null
-      })
-  })
-  newAnswers.forEach((row,i) =>{
-      row.forEach((col,j) => {
-          newAnswers[i][j] = _BOARD[i][j]
-      })
-  })
+  [" ", "9", " ", " ", "4", "2", "1", "3", "6"],
+  [" ", " ", " ", "9", "6", " ", "4", "8", "5"],
+  [" ", " ", " ", "5", "8", "1", " ", " ", " "],
 
-  let state = true;
-  while (state) {
-    let counter = 0;
-    for (row2 in newAnswers) {
-      for (col2 in newAnswers[row2]) {
-        fillInCell(newAnswers, row2, col2);
-        if (newAnswers[row2][col2] !== " ") {
-          counter++;
-        }
-        if (counter == 81) {
-          state = false;
-        }
+  [" ", " ", "4", " ", " ", " ", " ", " ", " "],
+  ["5", "1", "7", "2", " ", " ", "9", " ", " "],
+  ["6", " ", "2", " ", " ", " ", "3", "7", " "],
+
+  ["1", " ", " ", "8", " ", "4", " ", "2", " "],
+  ["7", " ", "6", " ", " ", " ", "8", "1", " "],
+  ["3", " ", " ", " ", "9", " ", " ", " ", " "],
+];
+
+const _COARDS = [
+  [1, 1, 1, 2, 2, 2, 3, 3, 3],
+  [1, 1, 1, 2, 2, 2, 3, 3, 3],
+  [1, 1, 1, 2, 2, 2, 3, 3, 3],
+
+  [4, 4, 4, 5, 5, 5, 6, 6, 6],
+  [4, 4, 4, 5, 5, 5, 6, 6, 6],
+  [4, 4, 4, 5, 5, 5, 6, 6, 6],
+
+  [7, 7, 7, 8, 8, 8, 9, 9, 9],
+  [7, 7, 7, 8, 8, 8, 9, 9, 9],
+  [7, 7, 7, 8, 8, 8, 9, 9, 9],
+];
+
+const getRow = function (board, row) {
+  //return an array with all the array elements from the row
+  return board[row];
+};
+
+const getColumn = function (board, column) {
+  //this return an array with all the array elements fom the column
+  let inColumn = [];
+  for (let row in board) {
+    inColumn.push(board[row][column]);
+  }
+  return inColumn;
+};
+const getQuad = function (board, quadNumber) {
+  //creates an array of all elements in the same quadrant
+  let inQuad = [];
+  //this cycles though all the rows
+  for (row in board) {
+    //this cycles though all the columns
+    for (col in board[row]) {
+      if (_COARDS[row][col] === quadNumber) {
+        inQuad.push(board[row][col]);
       }
     }
   }
-  console.table(newAnswers);
-   
-   
-   
-  
-  
+  return inQuad;
+};
+console.log(getQuad(_COARDS, 1));
+//filter through all the possible values and return all of the used values and return only the possible values of that cell
+const getPossible = function (board, row, col) {
+  let inColumn = getColumn(board, col);
+  let inRow = getRow(board, row);
+  let inQuad = getQuad(board, _COARDS[row][col]);
+  let possible = new Array(9).fill("").map((_, i) => {
+    return i + 1;
+  });
+
+  let notPossible = [];
+
+  //checking through the columns horizontally
+  inRow.forEach((curr, r) => {
+    let included = false;
+    notPossible.forEach((curr, p) => {
+      if (inRow[r] == notPossible[p]) {
+        included = true;
+      }
+    });
+    if (!included) {
+      notPossible.push(inRow[r]);
+    }
+  });
+
+  //checking through the columns vertically
+  inColumn.forEach((curr, c) => {
+    let included = false;
+    notPossible.forEach((curr, p) => {
+      if (inColumn[c] == notPossible[p]) {
+        included = true;
+      }
+    });
+    if (!included) {
+      notPossible.push(inColumn[c]);
+    }
+  });
+
+  // [(".", "9", "4", "2", "1", "3", "6", "5", "7")];  //checking quad
+  inQuad.forEach((curr, q) => {
+    let included = false;
+    notPossible.forEach((curr, p) => {
+      if (inQuad[q] == notPossible[p]) {
+        included = true;
+      }
+    });
+    if (!included) {
+      notPossible.push(inQuad[q]);
+    }
+  });
+
+  notPossible = notPossible.filter((curr) => curr !== " ");
+
+  notPossible.forEach((_, n) => {
+    possible.forEach((_, p) => {
+      if (notPossible[n] == possible[p]) {
+        possible.splice(p, 1);
+      }
+    });
+  });
+  return possible;
+};
+console.log(_BOARD);
+console.log(gridArr);
+const fillInCell = function (board, row, column) {
+  let possible = getPossible(board, row, column);
+
+  if (board[row][column] == " ") {
+    if (possible.length == 1) {
+      board[row][column] = possible[0];
+    }
+  }
+};
+let newAnswers = new Array(9).fill(0).map((row, i) => {
+  return new Array(9).fill(9).map((col) => {
+    return null;
+  });
+});
+newAnswers.forEach((row, i) => {
+  row.forEach((col, j) => {
+    newAnswers[i][j] = _BOARD[i][j];
+  });
+});
+
+let state = true;
+while (state) {
+  let counter = 0;
+  for (row2 in newAnswers) {
+    for (col2 in newAnswers[row2]) {
+      fillInCell(newAnswers, row2, col2);
+      if (newAnswers[row2][col2] !== " ") {
+        counter++;
+      }
+      if (counter == 81) {
+        state = false;
+      }
+    }
+  }
+}
+console.table(newAnswers);
+
 /**'
  *1st 
    create the board 
@@ -172,25 +168,21 @@ const _BOARD = [
    then i will create a 2d array of all the rows
  */
 
+const gridItems = function () {
+  new Array(81).fill("").forEach((_, i) => {
+    let styleBorder = "";
+    const div = document.createElement("div");
+    div.setAttribute("class", "col");
 
+    styleBorder +=
+      "border-bottom:1px solid #e2e2e2;border-right:1px solid #e2e2e2;";
 
+    div.style = styleBorder;
 
-const gridItems = function(){
-    new Array(81).fill('').forEach((_,i) => {
-        let styleBorder = ''
-        const div = document.createElement('div')
-        div.setAttribute('class','col');
-   
-         styleBorder +='border-bottom:1px solid #e2e2e2;border-right:1px solid #e2e2e2;'
- 
-        div.style = styleBorder
- 
-        gridContainer.append(div)
-    })
-
-}
-gridItems()
-
+    gridContainer.append(div);
+  });
+};
+gridItems();
 
 /*
 81 items
@@ -198,60 +190,66 @@ gridArr = [9 rows, 9 columns in each row ]
 */
 
 let rowMultiplyer = 0;
-let nodeArr = Array.from(gridContainer.querySelectorAll('.col'))
+let nodeArr = Array.from(gridContainer.querySelectorAll(".col"));
 
-gridArr.forEach((_,i) => {
-    gridArr.forEach((_,j) => {
-        gridArr[i][j] = nodeArr[j + rowMultiplyer]
+gridArr.forEach((_, i) => {
+  gridArr.forEach((_, j) => {
+    gridArr[i][j] = nodeArr[j + rowMultiplyer];
+  });
+  rowMultiplyer += 9;
+});
+const AddingCoardBorders = function () {
+  gridArr
+    .map((row, i) => {
+      let columns;
+      row.forEach((col, j) => {
+        if (j == 8) {
+          columns = col;
+        }
+      });
+      return columns;
     })
-    rowMultiplyer+=9
+    .forEach((curr) => {
+      curr.style.borderRight = "none";
+    });
 
-})
-const removeBorders = function(){
-    gridArr.map((row,i) => {
-        let columns
-         row.forEach((col,j) => {
-            if(j == 8){
-               columns = col
-            }
-        })
-        return columns
-    }).forEach(curr => {
-        curr.style.borderRight = 'none'
+  gridArr[8].forEach((curr) => {
+    curr.style.borderBottom = "none";
+  });
+
+  gridArr
+    .map((row, i) => {
+      let arr = row.filter((col, j) => {
+        if (j == 2 || j == 5) {
+          return col;
+        }
+      });
+      return arr;
     })
+    .flat()
+    .forEach((curr) => {
+      curr.style.borderRight = "3px solid #d3d3d4";
+    });
 
-    gridArr[8].forEach(curr => {
-        curr.style.borderBottom = 'none'
+  gridArr
+    .filter((row, i) => {
+      return i == 2 || i == 5;
     })
+    .flat()
+    .forEach((curr) => {
+      curr.style.borderBottom = "3px solid #d3d3d4";
+    });
+};
+AddingCoardBorders();
 
-     gridArr.map((row,i) => {
-        let arr = row.filter((col,j) => {
-            if(j ==2 || j ==5){
-                return col
-            }
-        })
-        return arr
-    }).flat().forEach((curr) => {
-        curr.style.borderRight = '3px solid #d3d3d4'
-
-    })
-
-     gridArr.filter((row,i) => {
-        return i == 2 || i == 5;
-    }).flat().forEach(curr => {
-        curr.style.borderBottom = '3px solid #d3d3d4'
-    })
-}
-removeBorders()
-
-let displayGame = function(board){
-    gridArr.forEach((row,i) => {
-        row.forEach((col,j) => {
-           col.textContent = board[i][j]            
-        })
-    })
-}
-displayGame(_BOARD)
+let displayGame = function (board) {
+  gridArr.forEach((row, i) => {
+    row.forEach((col, j) => {
+      col.textContent = board[i][j];
+    });
+  });
+};
+displayGame(_BOARD);
 
 /**
  * 
@@ -271,65 +269,206 @@ add the number also make sure they cant rechange the number
 
 if incorect make the background light red and remove number
 */
-const gridTextcontent = gridArr.map((row,i) => {
-    let currentRow = row.map((col,j) => {
-        return col.textContent
-    });
-    return currentRow
-})
-let clicked
+let gridTextcontent = gridArr.map((row, i) => {
+  let currentRow = row.map((col, j) => {
+    return col.textContent;
+  });
+  return currentRow;
+});
+let clicked = false;
 let correct;
-const clickSquare = function(){
-    let emptySquares = gridArr.map((row,i) => {
-        let columns = row.filter((col,j) => {
-            if(col.textContent == ' '){
-                return col
-            }
-        })
-        return columns
-    })
-    function eventSquare (e){
-        emptySquares.flat().forEach(current => {
-            current.style.backgroundColor = 'rgb(255, 255, 255)';
-        });
-        e.target.style.backgroundColor = 'rgb(255, 204, 238)';
-            clicked = e.target;
-         seletNum();
-        //  correct = 
-       correct = checkSquare();
+//this is giving is a array of the clickable squares
+let emptySquares = gridArr.map((row, i) => {
+  let columns = row.filter((col, j) => {
+    if (col.textContent == " ") {
+      return col;
     }
-  
-    emptySquares.flat().forEach(curr => {
-        curr.addEventListener('click',eventSquare);
+  });
+  return columns;
+});
+
+const clickSquare = function () {
+  emptySquares = gridArr.map((row, i) => {
+    let columns = row.filter((col, j) => {
+      if (col.textContent == " ") {
+        return col;
+      }
     });
+    return columns;
+  });
+  //click == true
+  //click ==false
+  emptySquares.flat().forEach((curr) => {
+    curr.addEventListener("mouseenter", (e) => {
+      if (clicked == false) {
+        emptySquares
+          .flat()
+          .forEach((current) => (current.style.backgroundColor = "white"));
+
+        e.target.style.backgroundColor = "#ffd6ee";
+      }
+    });
+  });
+
+  function eventSquare(e) {
+    emptySquares.flat().forEach((current) => {
+      current.style.backgroundColor = "rgb(255, 255, 255)";
+    });
+    clicked == false ? (clicked = e.target) : (clicked = false);
+    e.target.style.backgroundColor = "rgb(255, 204, 238)";
+    //  correct =
+    correct = checkSquare();
+    seletNum();
+  }
+
+  emptySquares.flat().forEach((curr) => {
+    curr.addEventListener("click", eventSquare);
+  });
 };
-const btns = Array.from(document.querySelectorAll('.btn'));
-const seletNum = function(){
-    if(clicked){
-        btns.forEach(curr => {
-            curr.addEventListener('click',function(e){
-                const num = e.target.textContent;
-                    if(String(correct) == curr.textContent){
-                        clicked.textContent = num
-                        clicked.style.color = '#ff52c5'
-                    }else{
-                        clicked.style.backgroundColor = '#fa9e9e'
-                    }
-            })
-        })
+
+function keyDown(e) {
+  console.log(e);
+  console.log("hi");
+}
+function updateBoard(col, text) {
+  let column = col;
+  console.log(column);
+  if (text) {
+    return (gridTextcontent = gridTextcontent.map((row, i) => {
+      if (column[0] == i) {
+        return row.map((col, j) => {
+          if (column[1] == j) {
+            return text;
+          }
+          return col;
+        });
+      }
+      return row;
+    }));
+  }
+  //2 arrays for the cols and values
+  //make the cols able to delete
+  return (gridTextcontent = gridTextcontent.map((row, i) => {
+    if (column[0] == i) {
+      return row.map((col, j) => {
+        if (column[1] == j) {
+          return "wrong";
+        }
+        return col;
+      });
     }
+    return row;
+  }));
 }
-const checkSquare = function(){
-    let coards = []
-    gridArr.forEach((row,i) => {
-        row.forEach((col,j) => {
-            if(col.style.backgroundColor == 'rgb(255, 204, 238)'){
-                coards.push(i,j)
-            }
-        })
-    })
-let answer = newAnswers[coards[0]][coards[1]]
-return answer
+const btns = Array.from(document.querySelectorAll(".btn"));
+//currentBoard
+let currentBoard;
+const seletNum = function () {
+  if (clicked) {
+    btns.forEach((curr) => {
+      curr.addEventListener("click", function (e) {
+        const num = e.target.textContent;
+        if (correct.includes(+num)) {
+          clicked.textContent = num;
+          clicked.style.color = "#d86b22";
+          updateBoard(checkSquare(false), clicked.textContent);
+          console.table(gridTextcontent);
+        } else {
+          8;
+          clicked.style.backgroundColor = "#fa9e9e";
+          console.log();
+        }
+      });
+    });
+    window.addEventListener("keydown", function (e) {
+      if (clicked) {
+        checkForIncorrect(gridTextcontent, checkSquare(false), e.key);
+      }
+      if (Number.parseInt(e.key)) {
+        if (correct.includes(+e.key)) {
+          clicked.textContent = e.key;
+          clicked.style.color = "#d86b22";
+          updateBoard(checkSquare(false), e.key);
+        } else {
+          clicked.style.backgroundColor = "#fa9e9e";
+          // updateBoard(checkSquare(false), clicked.textContent);
+        }
+      }
+    });
+  }
+};
+
+function checkSquare(answerArray = true) {
+  let coards = [];
+  gridArr.forEach((row, i) => {
+    row.forEach((col, j) => {
+      if (
+        col.style.backgroundColor == "#fa9e9e" ||
+        col.style.backgroundColor == "rgb(255, 204, 238)"
+      ) {
+        coards.push(i, j);
+      }
+    });
+  });
+
+  let answer = getPossible(gridTextcontent, coards[0], coards[1]);
+  if (answerArray) {
+    return answer;
+  }
+  gridTextcontent.forEach((row, i) => {
+    row.forEach((col, j) => {
+      if ((col.style.backgroundColor = "wrong")) {
+        coards.push(i, j);
+      }
+    });
+  });
+  return coards;
 }
-clickSquare()
-getPossible(_BOARD,0,2)
+clickSquare();
+
+function checkForIncorrect(board, coards, chekingAnswer) {
+  let [rowNumber, column] = coards;
+  console.log(coards);
+  //paramters will be the current board
+  //where are we going to check? each time we click on a square
+  //
+  //loop through the current board and get the cooards for the incorrect column
+  //loop throgh the row and find the incrrect number in the row
+  //when i have found it set bg to red
+  //loop trough column
+  //
+  //we will push the cols that are incorrect into array
+  //we are going to turn then bg color red
+
+  const checkRow = board.map((row, i) => {
+    if (i == rowNumber) {
+      return row.map((col, j) => {
+        if (row[i][j] == chekingAnswer) {
+          console.log(true);
+          return "foo";
+        }
+        return col;
+      });
+    }
+    return row;
+  });
+  console.log(checkRow);
+}
+
+//!!add the hover effect
+//!!add the unclick to the square
+//work on the the the board logoc
+/**
+ *!! instaed the user only selecting the correct answer i need to let him choose the options the the answer [8,2,1] he will find out later why that doesnt work
+ *
+ * if he chosses the inorect answer highlight the boxes to let him know it was incorrect
+ *
+ *
+ */
+//!!add key events to press keys
+//add different boards create select
+//add a timer
+//solve the whole sudoku btn
+//local storage
+
+//create modal in html with the rules
